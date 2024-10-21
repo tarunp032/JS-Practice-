31f6a4fcfc80c08be57a7c7cb4dd3627a1e3d6e0
tarunp032 <prajapattarun7568@gmail.com>
2024-10-21 22:11:59 +0530

    Kickstarting JavaScript Adventures: Set up the practice repo with exciting exercises to sharpen JS skills!

---
+++ script.js
@@ -0,0 +1,50 @@
+alert("Hello World")
+console.log("Hello World")
+console.log("code is running")
+console.log("code is also running")
+
+document.title= "Hey Tarun Here"
+
+// Data Types & Objects 
+let x= "tarun";
+let y= 23;
+let z= 34.5;
+const p=true;
+let q = undefined;
+let r= null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o = {
+    "name": "Ronak",
+    "job code": 5600,
+    "is_handsome":true
+}
+console.log(o);
+o.salary="100 crores";
+console.log(o);
+o.salary="500 crores";
+console.log(o);
+
+// Quick Quiz
+let t={
+    "name": "Tarun",
+    "Mobile Number":"7568447752",
+}
+console.log(t)
+t.marks="84%"
+console.log(t)
+
+//Practice set 
+var a=4;
+var b=9;
+console.log(a+b+8);
+console.log(typeof a,typeof b, typeof 8)
+var d="Tarun";
+console.log(d)
+console.log(typeof d)
+
+const f=6;
+console.log(f)
+console.log(typeof f)
